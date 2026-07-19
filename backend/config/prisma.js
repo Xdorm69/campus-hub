45ce45391fa8e43a5a7a8666ca1ff54f2d3e12db
amitoj <amitoj.mehta@gmail.com>
2026-07-19 17:11:37 +0530

    updated prisma

--- backend/config/prisma.js
+++ backend/config/prisma.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
-import { PrismaClient } from "../src/generated/prisma/client.ts";
-// Import the driver adapter for your specific database (example uses PostgreSQL)
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-// Initialize the adapter according to your driver's requirements
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-// Pass the adapter instance to PrismaClient
-const prisma = new PrismaClient({ adapter });
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
